c3a064388fa45205111991d9e3144fdfc4a92629
Wojciech Grabowski <w.w.grabowski@gmail.com>
2019-07-06 17:31:53 +0200

    Add exchange tests.

--- tests/test_currency_exchange.py
+++ tests/test_currency_exchange.py
@@ -1,5 +1,6 @@
 
 from unittest import TestCase
+from unittest.mock import Mock, MagicMock
 from python.currency_exchange import CurrencyExchange
 
 
@@ -12,19 +13,50 @@ class TestCurrencyExchange(TestCase):
         self.assertEqual(self.exchange_1.exchange_id, 1)
 
     def test_get_exchange_id(self):
-        pass
+        self.assertEqual(1, self.exchange_1.get_exchange_id())
 
     def test_store_operation(self):
-        pass
+        operation = Mock()
+
+        self.assertEqual(0, len(self.exchange_1._exchange_operations))
+        self.exchange_1.store_operation(operation)
+        self.exchange_1.store_operation(operation)
+        self.assertEqual(2, len(self.exchange_1._exchange_operations))
 
     def test_get_exchange_history(self):
-        pass
+        self.exchange_1._exchange_history.append('hist')
+        result = ['hist']
+
+        history = self.exchange_1.get_exchange_history()
+        self.assertEqual(result, history)
 
     def test_execute_operations(self):
-        pass
+        command = Mock()
+        command.execute = MagicMock()
+
+        self.exchange_1._exchange_operations = [command, command]
+        self.exchange_1.execute_operations()
+
+        self.assertEqual(2, command.execute.call_count)
+        operations = self.exchange_1._exchange_operations
+        self.assertEqual([], operations)
+        self.assertEqual([command, command], self.exchange_1._exchange_history)
 
     def test_get_exchange_product_by_id(self):
-        pass
+        product = Mock()
+        self.exchange_1._exchange_products['id'] = product
+
+        gathered_product = self.exchange_1.get_exchange_product_by_id('id')
+        self.assertEqual(product, gathered_product)
+
+    def test_get_exchange_product_by_id_exception(self):
+        self.assertRaises(KeyError,
+                          self.exchange_1.get_exchange_product_by_id('id'))
 
     def test_get_exchange_products(self):
-        pass
+        product = Mock()
+        self.exchange_1._exchange_products['id'] = product
+        result = {'id': product}
+
+        products = self.exchange_1.get_exchange_products()
+        self.assertEqual(result, products)
